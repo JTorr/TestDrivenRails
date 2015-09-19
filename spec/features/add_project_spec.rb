5a45fc9aa50d9a2acb74fe4ae38265f9dd8ae860
Julie Torres <reeddetorres@gmail.com>
2015-09-19 19:45:55 -0400

    Passing first end-to-end test

--- spec/features/add_project_spec.rb
+++ spec/features/add_project_spec.rb
@@ -6,7 +6,7 @@ describe "adding projects" do
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Task 1:3\nTask 2:5"
     click_on("Create Project")
-    visit_projects_path
+    visit projects_path
     expect(page).to have_content("Project Runway")
     expect(page).to have_content("8")
   end
